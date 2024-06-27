e000a4efc4ec4a47b23178023951891f7770ba56
725216 <djp.1@qq.com>
2024-06-27 10:11:09 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -15,8 +15,8 @@ git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci
 sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
 
 # 5-添加 PassWall 插件
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 git clone https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
 git clone https://github.com/muink/luci-app-homeproxy.git package/luci-app-homeproxy
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
