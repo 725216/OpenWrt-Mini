6a8fe2de51bc577494099d448d038568bc34f969
725216 <djp.1@qq.com>
2024-06-28 16:23:15 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -17,4 +17,4 @@ sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.con
 # 5-添加 PassWall 插件
 #echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-git clone https://github.com/gdy666/luci-app-lucky.git/lucky
+git clone https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
